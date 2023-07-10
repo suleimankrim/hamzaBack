1d840719d3db0a2d892ea9019ed19f08888a80b1
Hamza Shafee Aldaghstany <118183676+Hamza-Shafee-Aldaghstany@users.noreply.github.com>
2023-07-10 12:14:57 +0300

    SDSD

--- excelHelper/excel.js
+++ excelHelper/excel.js
@@ -1,11 +1,11 @@
-import xlsx from 'xlsx';
-const workBook= xlsx.readFile("../excelData/swp.xlsx")
-let workSheet= workBook.Sheets[workBook.SheetNames[0]];
+// import xlsx from 'xlsx';
+// const workBook= xlsx.readFile("../excelData/swp.xlsx")
+// let workSheet= workBook.Sheets[workBook.SheetNames[0]];
 
-export default  (req,res)=>{
-for (let i = 2; i < 5; i++) {
-   // for (let j = 2; j < 5; j++) {
-        console.log(workSheet[`B${i}`])
-    //}
+// export default  (req,res)=>{
+// for (let i = 2; i < 5; i++) {
+//    // for (let j = 2; j < 5; j++) {
+//         console.log(workSheet[`B${i}`])
+//     //}
 
-}}
+// }}
--- route/CellRoute.js
+++ route/CellRoute.js
@@ -1,6 +1,6 @@
  import express from "express";
  import * as cellController from "../controller/CellController.js";
- import sheet from "../excelHelper/excel.js"
+//  import sheet from "../excelHelper/excel.js"
 const router = express.Router();
 router.get('/cells', cellController.getAllCells);
 router.post('/cells', cellController.createCell);
@@ -8,5 +8,5 @@ router.get('/cells/:rowId/:colId', cellController.getCellByRowAndCol);
 router.post('/cells/:rowId/:colId', cellController.postCellByRowAndCol);
 router.get('/cells/row/:rowId', cellController.getAllCellsByRowId);
  router.get('/createTable', cellController.createEmptyCells);
- router.get('/e', sheet);
+//  router.get('/e', sheet);
 export default router;
